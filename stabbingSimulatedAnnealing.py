7ac6a3a3220193035d5f6aaea9018f9be3328282
AlexNeuhRUB <118838979+AlexNeuhRUB@users.noreply.github.com>
2022-11-28 19:55:04 +0100

    Update stabbingSimulatedAnnealing.py

--- stabbingSimulatedAnnealing.py
+++ stabbingSimulatedAnnealing.py
@@ -23,28 +23,28 @@ balls = []
 #print(track[1])
 
 r1 = .5
-c1 = np.array((0,0,0))
+c1 = np.array((0,0,1))
 r2 = .5
-c2 = np.array((1,0,0))
+c2 = np.array((1,0,1))
 r3 = .5
-c3 = np.array((2,0,0))
+c3 = np.array((2,0,1))
 r4 = .5
-c4 = np.array((3,0,0))
+c4 = np.array((3,4,2))
 r5 = .5
-c5 = np.array((4,0,0))
+c5 = np.array((4,0,2))
 r6 = .5
-c6 = np.array((5,0,0))
+c6 = np.array((5,0,2))
 r7 = .5
-c7 = np.array((6,0,0))
+c7 = np.array((6,0,3))
 r8 = .5
-c8 = np.array((7,0,0))
+c8 = np.array((7,0,3))
 r9 = .5
-c9 = np.array((8,0,0))
+c9 = np.array((8,4,4))
 r10 = .5
-c10 = np.array((9,0,0))
+c10 = np.array((9,0,4))
 
 
-balls = [[c1,r1],[c2,r2],[c3,r3],[c4,r4],[c5,r5],[c6,r6],[c6,r6],[c7,r7],[c8,r8],[c9,r9]]
+balls = [[c1,r1],[c2,r2],[c3,r3],[c4,r4],[c5,r5],[c6,r6],[c7,r7],[c8,r8],[c9,r9]]
 
 
 #function test data ends here
@@ -56,18 +56,18 @@ curve, sampleArray = stabbing_path(balls, n_samples)
 #print(curve)
 
 
-#c1,c2,p = convertCoordinates(np.array((0,0,1)),np.array((2,1,1)),np.array((0,1,1)))
+#c1,c2,p = (np.array((0,0,1)),np.array((2,1,1)),np.array((0,1,1)))
 #t1,t2 = outerTangents(.5,c1,1.5,c2)
+#print(checkContainment(c1,.5,c2,1.5,p))
 
 fig, ax = plt.subplots()
 #fig = plt.figure()
 #ax = Axes3D(fig)
+ax.set_xlim([0, 10])
+ax.set_ylim([-5, 5])
 
 c = list()
 
-for i in range(len(balls)):
-    c.append(plt.Circle((balls[i][0][0],balls[i][0][1]), balls[i][1]))
-    ax.add_patch(c[i])
 
 #plt.xlim(-2, 5)
 #plt.ylim(-2, 5)
@@ -81,6 +81,9 @@ for i in range(len(balls)):
 for i in range(len(sampleArray)):
     ax.scatter(sampleArray[i][:,0], sampleArray[i][:,1])
 ax.plot(curve[:,0], curve[:,1], color = 'black')
+for i in range(len(balls)):
+    c.append(plt.Circle((balls[i][0][0],balls[i][0][1]), balls[i][1]))
+    ax.add_patch(c[i])
 
 plt.show()
 #ax.set_aspect('equal', adjustable='datalim')
